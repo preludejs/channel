1164cf05f5379fbb692c72bad56ddc2d51a86515
Mirek Rusin <mirek@me.com>
2021-09-05 00:01:06 +0200

    Init.

---
+++ test/select.test.ts
@@ -0,0 +1,54 @@
+import * as Ch from '../index.js'
+
+test('select', async () => {
+  const a = Ch.of<number>()
+  const b = Ch.of<string>()
+  const results: string[] = []
+
+  const sink =
+    async function* () {
+      while (true) {
+        yield await Ch.select(a, b)
+      }
+    }
+
+  ;(async () => {
+    for await (const _ of sink()) {
+      results.push(`A   ${_.value}`)
+      if (Math.random() > 0.5) {
+        await new Promise(resolve => setTimeout(resolve, Math.round(Math.random() * 10)))
+      }
+    }
+  })()
+
+  ;(async () => {
+    for await (const _ of sink()) {
+      results.push(` B  ${_.value}`)
+      if (Math.random() > 0.5) {
+        await new Promise(resolve => setTimeout(resolve, Math.round(Math.random() * 10)))
+      }
+    }
+  })()
+
+  ;(async () => {
+    for await (const _ of sink()) {
+      results.push(`  C ${_.value}`)
+      if (Math.random() > 0.5) {
+        await new Promise(resolve => setTimeout(resolve, Math.round(Math.random() * 10)))
+      }
+    }
+  })()
+
+  for (let i = 0; i < 100; i++) {
+    if (Math.random() > 0.5) {
+      Ch.send(a, i)
+    } else {
+      Ch.send(b, i.toString())
+    }
+  }
+
+  await new Promise(resolve => setTimeout(resolve, 2000))
+
+  expect(results.length).toEqual(100)
+
+})
---
+++ test/simple.test.ts
@@ -0,0 +1,79 @@
+import * as Ch from '../index.js'
+import * as G from '@prelude/async-generator'
+
+test('simple', async () => {
+  const ch = Ch.of<number>()
+  const timeline: unknown[] = []
+
+  Ch.send(ch, 3).then(() => timeline.push([ 'sent', 3 ]))
+  Ch.send(ch, 5).then(() => timeline.push([ 'sent', 5 ]))
+  Ch.send(ch, 7).then(() => timeline.push([ 'sent', 7 ]))
+
+  for await (const value of G.take(3, ch)) {
+    timeline.push([ 'received', value ])
+  }
+
+  expect(timeline).toEqual([
+    [ 'sent', 3 ],
+    [ 'received', 3 ],
+    [ 'sent', 5 ],
+    [ 'received', 5 ],
+    [ 'sent', 7 ],
+    [ 'received', 7 ]
+  ])
+})
+
+test('delayed receive', async () => {
+
+  const delayedNumber =
+    (value: number) => {
+      const ch = Ch.of<number>()
+      setTimeout(() => {
+        Ch.send(ch, value)
+      }, Math.random() * 100)
+      return ch
+    }
+
+  const a = delayedNumber(3)
+  const b = delayedNumber(5)
+
+  const sum =
+    (x: number, y: number) =>
+      x + y
+
+  expect(sum(await Ch.receive(a), await Ch.receive(b))).toEqual(8)
+})
+
+
+test('delayed send', async () => {
+  const a = Ch.of<number>()
+  const b = Ch.of<number>()
+  const delayedSend =
+    (ch: Ch.t<number>, value: number) => {
+      setTimeout(() => {
+        Ch.send(ch, value)
+      }, Math.random() * 1000)
+    }
+  delayedSend(a, 3)
+  delayedSend(b, 5)
+  const sum =
+    (x: number, y: number) =>
+      x + y
+  expect(sum(await Ch.receive(a), await Ch.receive(b))).toEqual(8)
+})
+
+test('single delayed send', async () => {
+  const a = Ch.of<number>()
+  const delayedSend =
+    (value: number) => {
+      setTimeout(() => {
+        Ch.send(a, value)
+      }, Math.random() * 1000)
+    }
+  delayedSend(3)
+  delayedSend(5)
+  const sum =
+    (x: number, y: number) =>
+      x + y
+  expect(sum(await Ch.receive(a), await Ch.receive(a))).toEqual(8)
+})
